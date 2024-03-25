4d44fa2f32ab56a4ef4dc4e60b91984b20e1e6f9
LuckyKhoza-crypto <khozal@whitman.edu>
2024-03-24 23:20:41 -0700

    added site counter

--- gamenationApp/models.py
+++ gamenationApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class visit_count(models.Model):
+    url = models.CharField(max_length=200, unique=True)
+    count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.url
--- gamenationApp/views.py
+++ gamenationApp/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import visit_count
 
 # Create your views here.
 
+
 def home(request):
-    return render(request, 'home.html', {})
+
+    # get the current URL
+    current_url = request.build_absolute_uri()
+
+    # get the object or create it if it doesn't exist
+    count_value, created = visit_count.objects.get_or_create(url=current_url)
+
+    if not created:
+        count_value.count += 1
+        count_value.save()
+
+    return render(request, 'home.html', {'count_value': count_value.count})
