10f527446cbd21455d11fdb769889c8bff23af40
LuckyKhoza-crypto <84252978+LuckyKhoza-crypto@users.noreply.github.com>
2024-03-26 01:16:06 -0700

    Update urls.py

--- gamenationApp/urls.py
+++ gamenationApp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('home', home, name='home'),
+    path('', home, name='home'),
     path('import/', ImportTrafficSource.as_view(), name='import'),
     path('visual/', traffic_source_visual.as_view(), name='visual'),
     path('api/', traffic_source_api.as_view(), name='api'),
